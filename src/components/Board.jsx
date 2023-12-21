dcf4517c437130ee0ea075e58c3651864b2a74a7
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-22 00:37:56 +0530

    dispay X when clicking on a aquare

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+
+  const handleClick = (clickPosition) => {
+    setState((currentClickPosition) => {
+      return currentClickPosition.map((squareVal, position) => {
+        if (clickPosition === position) {
+          return "X";
+        }
+        return squareVal;
+      });
+    });
+  };
+
+  const renderSquare = (position) => {
+    return (
+      <Square value={state[position]} onClick={() => handleClick(position)} />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4}/>
-        <Square value={5}/>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6}/>
-        <Square value={7}/>
-        <Square value={8}/>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
