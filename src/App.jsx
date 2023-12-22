4eabc181610c9a392d29c05af026131adf9f632a
Siddhi <bandivdekarsiddhi@gmail.com>
2023-12-22 20:47:59 +0530

    display draw message. apply styles for StatusMessage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import "./components/styles.scss";
 import { useState } from "react";
 import Board from "./components/Board";
 import { calculatedWinner } from "./components/winner";
+import StatusMessage from "./components/StatusMessage";
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
 
   const winner = calculatedWinner(squares);
-  const nextPlayer = isXNext ? "X" : "0";
-  const statusMessage = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${nextPlayer}`;
-
   const handleClick = (clickPosition) => {
     if (squares[clickPosition] || winner) {
       return;
@@ -31,7 +27,9 @@ const App = () => {
 
   return (
     <div className="app">
-      <h2>{statusMessage}</h2>
+      <h2>
+        <StatusMessage winner={winner} isXNext={isXNext}  squares={squares}/>
+      </h2>
       <Board handleClick={handleClick} squares={squares} />
     </div>
   );
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,41 @@
+const StatusMessage = ({ winner, isXNext, squares }) => {
+  const nextPlayer = isXNext ? "X" : "0";
+
+  const noMovesLeft = squares.every((squareVal) => squareVal !== null);
+
+  const renderMessage = () => {
+    if (winner) {
+      return (
+        <div>
+          Winner is{" "}
+          <span className={isXNext ? "text-green" : "text-orange"}>
+            {winner}
+          </span>
+        </div>
+      );
+    }
+    if (!winner && noMovesLeft) {
+      return (
+        <div>
+          <span className="text-green">X</span> and{" "}
+          <span className="text-orange">0</span> have tied!
+        </div>
+      );
+    }
+    if (!winner && !noMovesLeft) {
+      return (
+        <div>
+          Next player is{" "}
+          <span className={isXNext ? "text-green" : "text-orange"}>
+            {nextPlayer}
+          </span>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return <div className="status-message">{renderMessage()}</div>;
+};
+
+export default StatusMessage;
